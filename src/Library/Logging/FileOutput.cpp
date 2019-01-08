1509fe797c4cc8937c269ecb94541375fe2b3482
jeanp413 <jeanp413@hotmail.com>
2019-01-08 16:10:08 -0500

    Added  FileOutput logging target

---
+++ src/Library/Logging/FileOutput.cpp
@@ -0,0 +1,29 @@
+#include "Library/Logging/FileOutput.h"
+#include "Library/Logging/Logger.h"
+#include <iostream>
+
+namespace Library {
+    namespace Logging {
+        FileOutput::FileOutput(const std::string& filename, bool truncate) {
+            auto mode = (truncate ? (std::ios::out | std::ios::app | std::ios::trunc) : (std::ios::out | std::ios::app)); 
+            file.open(filename, mode);
+        }
+
+        FileOutput::~FileOutput() {
+        }
+
+        void FileOutput::flush(std::string&& log) {
+            std::unique_lock<std::mutex> lock(mutex);
+            file << log << std::endl;
+        }
+
+        void FileOutput::flush(const std::string& log) {
+            std::unique_lock<std::mutex> lock(mutex);
+            file << log << std::endl;
+        }
+
+        void FileOutput::setNodeIdentifier(const unsigned int nodeInd){
+        	Logging::Logger().logInfo("Node index is " + std::to_string(nodeInd));
+        }
+    }
+}
