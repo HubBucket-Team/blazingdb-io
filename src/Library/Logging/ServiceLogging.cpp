9de9e248be3241cdfea4831ba348814442b84cdd
felipe <felipe@blazingdb.com>
2019-01-13 04:15:10 -0600

    adding insanity test

--- src/Library/Logging/ServiceLogging.cpp
+++ src/Library/Logging/ServiceLogging.cpp
@@ -1,12 +1,14 @@
 #include "Library/Logging/ServiceLogging.h"
 #include "Library/Logging/GenericOutput.h"
 #include "Library/Logging/FileOutput.h"
+#include <iostream>
 
 namespace Library {
     namespace Logging {
         ServiceLogging::ServiceLogging() {
     		//allows unit tests to run dumping to some log
 		auto output = new FileOutput("tmp/blazing-logger.log", true);
+		std::cout<<"insanity test"<<std::endl;
     		setLogOutput(output);
         }
 
